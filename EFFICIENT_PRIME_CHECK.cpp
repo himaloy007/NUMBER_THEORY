98a657afe354f213f84d7318c68e9a9c2bf72244
himaloy007 <140476296+himaloy007@users.noreply.github.com>
2023-08-15 01:51:41 +0600

    NUMBER_THEORY_001

---
+++ EFFICIENT_PRIME_CHECK.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+bool EfficientPrimeCheck(ll n)
+{
+    if (n < 2)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0)
+        return false;
+
+    for (ll i = 3; i * i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    ll n;
+    cin >> n;
+    cout << EfficientPrimeCheck(n);
+
+}
